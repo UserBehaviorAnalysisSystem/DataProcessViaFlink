a681701706e32b27b05bf3b0991c1cc1beffcc7a
zztttt <1315691850@qq.com>
2020-03-22 20:18:51 +0800

    update main

--- src/main/java/cn/zzt/Main.java
+++ src/main/java/cn/zzt/Main.java
@@ -2,6 +2,9 @@ package cn.zzt;
 import cn.zzt.MyClass;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.windowing.time.Time;
+import cn.AggregateFunction.CountSpecificKey;
+import cn.WindowFunction.WindowResultFunction;
+import cn.KeyedProcessFunction.TopNHotItems;
 
 public class Main {
     public static void main(String[] args) throws Exception{
@@ -16,13 +19,13 @@ public class Main {
         DataStream<ItemViewCount> windowedData = pv
                 .keyBy("itemId")
                 .timeWindow(Time.minutes(60), Time.minutes(5))
-                .aggregate(new function.CountAgg(), new function.WindowResultFunction());
+                .aggregate(new CountSpecificKey(), new WindowResultFunction());
         /*
          * 以windowEnd来分组，然后实现一个TopHotItem函数，并将结果格式化为字符串
          */
         DataStream<String> topItems = windowedData
                 .keyBy("windowEnd")
-                .process(new function.TopNHotItems(4));  // 求点击量前4名的商品
+                .process(new TopNHotItems(4));  // 求点击量前4名的商品
 
         topItems.print();
         m.env.execute("Hot Items Job");
