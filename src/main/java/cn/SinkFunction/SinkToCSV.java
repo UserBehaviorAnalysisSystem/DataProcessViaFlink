3f275e86e1131846cf6f947bb0e24b885ca8bda7
zztttt <1315691850@qq.com>
2020-03-23 23:43:14 +0800

    update PV/UV and type: type/all result

--- src/main/java/cn/SinkFunction/SinkToCSV.java
+++ src/main/java/cn/SinkFunction/SinkToCSV.java
@@ -2,25 +2,55 @@ package cn.SinkFunction;
 
 import cn.zzt.UserBehavior;
 import org.apache.flink.streaming.api.functions.sink.RichSinkFunction;
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 
 /* extends AbstractRichFunction */
-public class SinkToCSV extends RichSinkFunction<HashMap<Long, Long>> {
+public class SinkToCSV extends RichSinkFunction<HashSet<UserBehavior>> {
     int count = 0;
     //HashMap<Long, Long> ret = new HashMap<>();
     @Override
-    public void invoke(HashMap<Long, Long> map, Context context) throws Exception {
-        //System.out.println("map size:" + String.valueOf(map.size()));
-        //System.out.printf("count:%d\n", count++);
-        for(Map.Entry<Long, Long> e: map.entrySet()){
-            if(e.getValue() > 1L){
-                //ret.put(e.getKey(), e.getValue());
-                StringBuilder sb = new StringBuilder();
-                sb.append(e.getKey()).append(":").append(e.getValue());
-                System.out.println(sb.toString());
+    public void invoke(HashSet<UserBehavior> set, Context context) throws Exception {
+
+        //String windowStart = new DateTime(context.window().getStart(), DateTimeZone.forID("+08:00")).toString("yyyy-MM-dd HH:mm:ss");
+        // userId - count
+        HashMap<Long, Long> map = new HashMap<>();
+        // operator type - count
+        HashMap<String, Long> operatorMap = new HashMap<>();
+        float count = 0, onlyPv = 0;
+        Long windowEnd = Long.MIN_VALUE;
+        for(UserBehavior u: set){
+            count++;
+            Long id = u.getUserId();
+            String type = u.behavior;
+            Long time = u.timestamp;
+
+            if(time > windowEnd)
+                windowEnd = time;
+
+            if(map.containsKey(id)){
+                Long old = map.get(id);
+                map.put(id, old + 1);
+            }else{
+                map.put(id, 1L);
             }
+
+            if(operatorMap.containsKey(type)){
+                Long old = operatorMap.get(type);
+                operatorMap.put(type, old + 1);
+            }else{
+                operatorMap.put(type, 1L);
+            }
+        }
+        int allUser = map.size();
+        System.out.printf("PV/UV: %f\n", count / allUser);
+        for(Map.Entry<String, Long> e: operatorMap.entrySet()){
+            Long cur = e.getValue();
+            System.out.printf("%s/all: %d/%f = %f\n", e.getKey(), e.getValue(), count, e.getValue() / count);
         }
 
     }
--- src/main/java/cn/WindowFunction/ProcessCountUser.java
+++ src/main/java/cn/WindowFunction/ProcessCountUser.java
@@ -6,26 +6,21 @@ import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
 import org.apache.flink.util.Collector;
 
 import java.util.HashMap;
+import java.util.HashSet;
 
 /* ProcessWindowFunction<IN, OUT, KEY, W extends Window> */
 /* ProcessAllWindowFunction<IN, OUT, W extends Window> */
-public class ProcessCountUser extends ProcessAllWindowFunction<UserBehavior, HashMap<Long, Long>, TimeWindow> {
+public class ProcessCountUser extends ProcessAllWindowFunction<UserBehavior, HashSet<UserBehavior>, TimeWindow> {
     @Override
-    public void process(Context context, Iterable<UserBehavior> input, Collector<HashMap<Long, Long>> out){
+    public void process(Context context, Iterable<UserBehavior> input, Collector<HashSet<UserBehavior>> out){
         //String windowStart=new DateTime(context.window().getStart(), DateTimeZone.forID("+08:00")).toString("yyyy-MM-dd HH:mm:ss");
         //String windowEnd=new DateTime(context.window().getEnd(), DateTimeZone.forID("+08:00")).toString("yyyy-MM-dd HH:mm:ss");
         String windowTime = context.window().toString();
         System.out.println(windowTime);
 
-        HashMap<Long, Long> ret = new HashMap<>();
+        HashSet<UserBehavior> ret = new HashSet<>();
         for(UserBehavior u: input){
-            Long userId = u.userId;
-            if(ret.containsKey(userId)){
-                Long old = ret.get(userId);
-                ret.put(userId, old + 1);
-            }else{
-                ret.put(userId, 1L);
-            }
+            ret.add(u);
         }
         out.collect(ret);
     }
